a15629e4c9293c0cb1ef527b7538cfb3a6510bf9
gsjang94@gamil.com <gsjang94@gmail.com>
2023-05-31 19:00:42 +0900

    타입스크립트 props

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,9 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Todos from './components/Todos';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Todos items={['리액트 배우기', '타입스크립트 배우기', '딥다이브 공부','메인 프로젝트 기획']} />
     </div>
   );
 }
