b45e2b49c089ee7506631383c4b553e56a07d3e2
gsjang94@gamil.com <gsjang94@gmail.com>
2023-06-01 16:11:26 +0900

    삭제 추가

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,7 +1,7 @@
 import classes from './TodoItem.module.css'
 
-const TodoItem: React.FC<{ todoText: string }> = (props) => {
-  return <li className={classes.item}>{props.todoText}</li>;
+const TodoItem: React.FC<{ todoText: string; onRemoveTodo:()=>void }> = (props) => {
+  return <li className={classes.item} onClick={props.onRemoveTodo}>{props.todoText}</li>;
 };
 
 export default TodoItem;
